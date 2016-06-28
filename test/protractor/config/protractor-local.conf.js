cccaab98a806f5ef5e67f6f201e8bc308dd1ea27
Mischa Dasberg <mischa@dasberg.nl>
2016-06-28 21:23:25 +0200

    Rewrite to cucumber

--- test/protractor/config/protractor-local.conf.js
+++ test/protractor/config/protractor-local.conf.js
@@ -2,12 +2,12 @@ var config = require('./protractor-shared.conf').config;
 
 config.seleniumAddress = 'http://localhost:4444/wd/hub';
 
-config.specs = [
-    '../**/*.spec.js'
-];
-
 config.multiCapabilities = [
-    {'browserName': 'chrome'}
+    {
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 10
+    }
 ];
 
 exports.config = config;
