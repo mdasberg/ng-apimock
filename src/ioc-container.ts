cb5bc9aa4f71d19a22f18773a142939072734856
Mischa Dasberg <mischa@dasberg.nl>
2018-01-10 21:30:14 +0100

    feat(ioc): Add ioc container

---
+++ src/ioc-container.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import {Container} from 'inversify';
+
+import EchoRequestHandler from './middleware/handlers/mock/echo.request.handler';
+import MockRequestHandler from './middleware/handlers/mock/mock.request.handler';
+import RecordResponseHandler from './middleware/handlers/mock/record.response.handler';
+import ScenarioHandler from './middleware/handlers/api/scenario.handler';
+import ActionHandler from './middleware/handlers/api/action.handler';
+import MocksState from './state/mocks.state';
+import MocksProcessor from './processor/processor';
+import Middleware from './middleware/middleware';
+
+// IOC configuration
+const container = new Container();
+
+container.bind<MocksState>('MocksState').to(MocksState).inSingletonScope();
+
+container.bind<EchoRequestHandler>('EchoRequestHandler').to(EchoRequestHandler);
+container.bind<MockRequestHandler>('MockRequestHandler').to(MockRequestHandler);
+container.bind<RecordResponseHandler>('RecordResponseHandler').to(RecordResponseHandler);
+
+container.bind<ScenarioHandler>('ScenarioHandler').to(ScenarioHandler);
+container.bind<ActionHandler>('ActionHandler').to(ActionHandler);
+
+container.bind<MocksProcessor>('MocksProcessor').to(MocksProcessor);
+container.bind<Middleware>('Middleware').to(Middleware);
+
+export default container;
