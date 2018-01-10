13087901fe1feaa0ce2ba5d073dd20f640c418f0
Mischa Dasberg <mischa@dasberg.nl>
2018-01-10 21:29:49 +0100

    feat(middleware): Add middleware

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,84 @@
+import 'reflect-metadata';
+import {inject, injectable} from 'inversify';
+
+import * as http from 'http';
+
+import ActionHandler from './handlers/api/action.handler';
+import EchoRequestHandler from './handlers/mock/echo.request.handler';
+import {HttpMethods} from './http';
+import Mock from '../domain/mock';
+import MockRequestHandler from './handlers/mock/mock.request.handler';
+import MocksState from '../state/mocks.state';
+import RecordResponseHandler from './handlers/mock/record.response.handler';
+import ScenarioHandler from './handlers/api/scenario.handler';
+
+/** Middleware. */
+@injectable()
+class Middleware {
+
+    @inject('MockRequestHandler')
+    private mockRequestHander: MockRequestHandler;
+
+    @inject('EchoRequestHandler')
+    private echoRequestHandler: EchoRequestHandler;
+
+    @inject('RecordResponseHandler')
+    private recordResponseHandler: RecordResponseHandler;
+
+    @inject('ScenarioHandler')
+    private scenarioHandler: ScenarioHandler;
+
+    @inject('ActionHandler')
+    private actionHandler: ActionHandler;
+
+    @inject('MocksState')
+    private apimockState: MocksState;
+
+    /**
+     * Apimock Middleware.
+     * @param {"http".IncomingMessage} request The request.
+     * @param {"http".ServerResponse} response The response.
+     * @param {Function} next The next callback function.
+     */
+    middleware(request: http.IncomingMessage, response: http.ServerResponse, next: Function): void {
+        const apimockId: string = this.getApimockId(request.headers);
+        const matchingMock: Mock = this.apimockState.getMatchingMock(request.url, request.method);
+
+        if (request.url.startsWith('/ngapimock/mocks')) {
+            this.scenarioHandler.handle(request, response, next, apimockId);
+        } else if (request.url.startsWith('/ngapimock/actions') && request.method === HttpMethods.PUT) {
+            this.actionHandler.handle(request, response, next, apimockId);
+        } else if (matchingMock !== undefined) {
+            this.echoRequestHandler.handle(request, response, next, apimockId, matchingMock);
+            if (this.apimockState.record && !request.headers.record) {
+                this.recordResponseHandler.handle(request, response, next, apimockId, matchingMock);
+            } else {
+                this.mockRequestHander.handle(request, response, next, apimockId, matchingMock);
+            }
+        } else {
+            next();
+        }
+    }
+
+    /**
+     * Get the apimockId from the given cookies.
+     * @param headers The headers.
+     * @returns {string} id The apimock id.
+     */
+    getApimockId(headers: http.IncomingHttpHeaders): string {
+        const result = headers.cookie && (headers.cookie as string)
+            .split(';')
+            .map(cookie => {
+                const parts = cookie.split('=');
+                return {
+                    key: parts.shift().trim(),
+                    value: decodeURI(parts.join('='))
+                };
+            })
+            .find(cookie => cookie.key === 'apimockid');
+
+        return result !== undefined ? result.value : undefined;
+    }
+}
+
+export default Middleware;
