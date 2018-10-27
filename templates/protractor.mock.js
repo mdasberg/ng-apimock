6d7a3318acf2664b87e035f03eed39ab9ea4fef6
Mischa Dasberg <mischa@dasberg.nl>
2018-10-27 10:20:13 +0200

    refactor(protractor-mock): Remove legacy protrator version support

--- templates/protractor.mock.js
+++ templates/protractor.mock.js
@@ -44,13 +44,10 @@
             require('hooker').hook(browser, 'get', {
                 post: function (result) {
                     return result.then(function () {
-                        // Since protractor 5.0.0 the addCookie is an object, see
-                        // https://github.com/angular/protractor/blob/master/CHANGELOG.md#500
                         try {
                             return browser.manage().addCookie({name: "ngapimockid", value: ngapimockid});
                         } catch (error) {
-                            // Fallback protractor < 5.0.0
-                            return browser.manage().addCookie('ngapimockid', ngapimockid);
+                            console.log(error);
                         }
                     });
                 }
